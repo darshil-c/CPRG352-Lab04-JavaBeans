c3e61ac351c8cc9270a2e848868319237005df67
Chaudhari <Chaudhari@DESKTOP-M3C7DC6>
2021-06-07 11:58:12 -0600

    Complete Without Comments Working Lab 4 without comments

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -7,8 +7,10 @@ package servlets;
 
 import domain.Note;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -21,20 +23,28 @@ import javax.servlet.http.HttpServletResponse;
  * @author Chaudhari
  */
 public class NoteServlet extends HttpServlet {
-    
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String path = getServletContext().getRealPath("/WEB-INF/note.txt");
         BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-        String title = br.readLine();
-        String content = br.readLine();
-        
-        Note note = new Note(title, content);
-        
+        String titleQuery = request.getParameter("title");
+        String contentQuery = request.getParameter("content");
+        Note note;
+
+        if (titleQuery == null && contentQuery == null) {
+            String title = br.readLine();
+            String content = br.readLine();
+            note = new Note(title, content);
+        } else {
+            note = new Note(titleQuery, contentQuery);
+        }
+
+       
+
         request.setAttribute("note", note);
-        
+
         String editNote = request.getParameter("edit");
 
         if (editNote == null) {
@@ -49,7 +59,12 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+        String title = request.getParameter("title");
+        String content = request.getParameter("content");
+        pw.print(title);
+        pw.println(content);
     }
 
 }
